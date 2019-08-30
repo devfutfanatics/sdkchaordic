c935ef3883bbf9dad41cc7be00a71e20e1f3e975
Weslley Ceresa <weslley@futcorp.com.br>
2019-08-30 14:03:38 -0300

    Metodos de busca

--- src/Search.php
+++ src/Search.php
@@ -3,14 +3,41 @@
 namespace SdkChaordic;
 
 use SdkChaordic\Core\BaseSdk;
+use SdkChaordic\Library\Http;
 
 class Search extends BaseSdk{
     private $http;    
     
     public function __construct() {
         $this->http = new Http();
-        $this->http->setBaseUrl("https://api.linximpulse.com/engage/search/v3/");
+        $this->http->setBaseUrl("https://busca.futfanatics.com.br/searchapi/v3/");
     }
     
-    
+    public function search($terms, $page, $resultsPerPage, $sortBy, array $filters, $pids, $salesChannel, $hide, $productFormat, $showOnlyAvailable, $allowRedirect){
+        $filter = array();     
+        
+        foreach($filters as $index => $f){
+            if($index == 0){
+                $filter[] = $f;
+            }
+            else{
+                $filter[] = "&filter=" . $f;
+            }
+        }
+        
+        $response = $this->http->get("search", array(
+            "apiKey" => $this->apiKey,
+            "secretKey" => $this->apiSecret,
+            "resultsPerPage" => $resultsPerPage,
+            "page" => $page,
+            "sortBy" => $sortBy,
+            "terms" => $terms,
+            "filter" => $filter
+        ));
+        
+        if($this->isSucess($response["code"]))
+            return $response["data"];
+        
+        throw new SdkChaordicException("[Search][search] " . $response["responseText"]);
+    }
 }
