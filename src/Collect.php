6a3a37d45089cbef1eeec1fa39a4699cfe93b109
Weslley Ceresa <weslley@futcorp.com.br>
2019-09-02 16:53:30 -0300

    Metodos para coleta

---
+++ src/Collect.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace SdkChaordic;
+
+use SdkChaordic\Core\BaseSdk;
+use SdkChaordic\Exceptions\SdkChaordicException;
+use SdkChaordic\Library\Http;
+
+class Collect extends BaseSdk{
+    private $http;    
+    
+    public function __construct() {
+        $this->http = new Http();
+        $this->http->setBaseUrl("https://collect.chaordicsystems.com/v7/events/views/");
+    }
+    
+    public function product($source, $deviceId, $pid, $user = null){
+        $body = array(
+            "apiKey" => $this->apiKey,
+            "secretKey" => $this->apiSecret,
+            "source" => $source,
+            "deviceId" => $deviceId,
+            "pid" => $pid
+        );
+        
+        if($user){
+            $body["user"] = $user;
+        }
+        
+        $response = $this->http->post("product", null, $body);
+        
+        if($this->isSucess($response["code"]))
+            return $response["data"];
+        
+        throw new SdkChaordicException("[Collect][product] " . $response["responseText"]);
+    }
+    
+    public function cart($source, $deviceId, array $items, $user = null){
+        $body = array(
+            "apiKey" => $this->apiKey,
+            "secretKey" => $this->apiSecret,
+            "source" => $source,
+            "deviceId" => $deviceId,
+            "items" => $items
+        );
+        
+        if($user){
+            $body["user"] = $user;
+        }
+        
+        $response = $this->http->post("cart", null, $body);
+        
+        if($this->isSucess($response["code"]))
+            return $response["data"];
+        
+        throw new SdkChaordicException("[Collect][cart] " . $response["responseText"]);
+    }
+    
+    public function transaction($source, $deviceId, array $items, $id, $total, $user = null){
+        $body = array(
+            "apiKey" => $this->apiKey,
+            "secretKey" => $this->apiSecret,
+            "source" => $source,
+            "deviceId" => $deviceId,
+            "items" => $items,
+            "id" => $id
+        );
+        
+        if($user){
+            $body["user"] = $user;
+        }
+        
+        $response = $this->http->post("transaction", null, $body);
+        
+        if($this->isSucess($response["code"]))
+            return $response["data"];
+        
+        throw new SdkChaordicException("[Collect][transaction] " . $response["responseText"]);
+    }
+    
+    public function search($source, $deviceId, array $items, $query, $user = null){
+        $body = array(
+            "apiKey" => $this->apiKey,
+            "secretKey" => $this->apiSecret,
+            "source" => $source,
+            "deviceId" => $deviceId,
+            "items" => $items,
+            "query" => $query
+        );
+        
+        if($user){
+            $body["user"] = $user;
+        }
+        
+        $response = $this->http->post("search", null, $body);
+        
+        if($this->isSucess($response["code"]))
+            return $response["data"];
+        
+        throw new SdkChaordicException("[Collect][search] " . $response["responseText"]);
+    }
+    
+    public function category($source, $deviceId, array $categories, $query, $user = null){
+        $body = array(
+            "apiKey" => $this->apiKey,
+            "secretKey" => $this->apiSecret,
+            "source" => $source,
+            "deviceId" => $deviceId,
+            "categories" => $categories
+        );
+        
+        if($user){
+            $body["user"] = $user;
+        }
+        
+        $response = $this->http->post("category", null, $body);
+        
+        if($this->isSucess($response["code"]))
+            return $response["data"];
+        
+        throw new SdkChaordicException("[Collect][category] " . $response["responseText"]);
+    }
+}
