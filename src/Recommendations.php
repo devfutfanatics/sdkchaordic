6f440f5065326e9bbad7b1fab683e7ede9ed5e87
Weslley Ceresa <weslley@futcorp.com.br>
2019-10-14 09:58:42 -0300

    Aumentar suporte do SDK a recomendação em outras paginas

--- src/Recommendations.php
+++ src/Recommendations.php
@@ -14,15 +14,59 @@ class Recommendations extends BaseSdk{
         $this->http->setBaseUrl("https://recs.chaordicsystems.com/v0/");
     }
     
-    public function pages($name, $source, $deviceId, $productFormat){
-        $response = $this->http->get("pages/recommendations", array(
+    public function pages($name, $source, $deviceId, $productFormat, $categoryId = null, $productId = null){
+        $query = array(
             "apiKey" => urlencode($this->apiKey),
             "secretKey" => urlencode($this->apiSecret),
             "name" => urlencode($name),
             "source" => urlencode($source),
             "deviceId" => urlencode($deviceId),
             "productFormat" => urlencode($productFormat)
-        ));
+        );
+        
+        if($categoryId){
+            if(is_array($categoryId)){
+                $categories = array();
+
+                foreach($categoryId as $index => $f){
+                    if($index == 0){
+                        $categories[] = urlencode($f);
+                    }
+                    else{
+                        $categories[] = "categoryId=" . urlencode($f);
+                    }
+                }
+                
+                $query["categoryId"] = implode("&", $categories);
+                
+            }
+            else{
+                $query["categoryId"] = $categoryId;
+            }
+        }
+        
+        if($productId){
+            if(is_array($productId)){
+                $products = array();
+
+                foreach($productId as $index => $f){
+                    if($index == 0){
+                        $products[] = urlencode($f);
+                    }
+                    else{
+                        $products[] = "productId=" . urlencode($f);
+                    }
+                }
+                
+                $query["productId"] = implode("&", $products);
+                
+            }
+            else{
+                $query["productId"] = $productId;
+            }
+        }
+        
+        $response = $this->http->get("pages/recommendations", $query);
         
         if($this->isSucess($response["code"]))
             return $response["data"];
